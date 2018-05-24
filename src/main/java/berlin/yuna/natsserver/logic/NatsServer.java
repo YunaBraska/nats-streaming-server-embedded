3abbb89cde21c8d7ed0e3dfe46dc76c25063c51e
yuna90 <yuna-@web.de>
2018-05-24 09:16:04 +0200

    feature/fixTestClosingPort jacoco fix

--- src/main/java/berlin/yuna/natsserver/logic/NatsServer.java
+++ src/main/java/berlin/yuna/natsserver/logic/NatsServer.java
@@ -2,7 +2,6 @@ package berlin.yuna.natsserver.logic;
 
 import berlin.yuna.natsserver.util.SystemUtil.OperatingSystem;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.util.StringUtils;
 
@@ -24,10 +23,14 @@ import java.util.ServiceConfigurationError;
 import static berlin.yuna.natsserver.util.SystemUtil.OperatingSystem.WINDOWS;
 import static berlin.yuna.natsserver.util.SystemUtil.getOsType;
 import static java.nio.file.attribute.PosixFilePermission.GROUP_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.GROUP_READ;
 import static java.nio.file.attribute.PosixFilePermission.OTHERS_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.OTHERS_READ;
 import static java.nio.file.attribute.PosixFilePermission.OWNER_EXECUTE;
+import static java.nio.file.attribute.PosixFilePermission.OWNER_READ;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * {@link NatsServer}
@@ -39,8 +42,8 @@ import static java.util.concurrent.TimeUnit.SECONDS;
  */
 public class NatsServer implements DisposableBean {
 
-    public static final String name = NatsServer.class.getSimpleName();
-    private static final Logger LOG = LoggerFactory.getLogger(NatsServer.class);
+    public static final String BEAN_NAME = NatsServer.class.getSimpleName();
+    private static final Logger LOG = getLogger(NatsServer.class);
     private static final String NATS_SERVER_VERSION = "v0.9.2";
     private static final OperatingSystem OPERATING_SYSTEM = getOsType();
 
@@ -49,13 +52,13 @@ public class NatsServer implements DisposableBean {
     private Process process;
 
     /**
-     * Create non start able {@link NatsServer} without any configuration
+     * Create {@link NatsServer} without any start able configuration
      */
     public NatsServer() {
     }
 
     /**
-     * Create start able {@link NatsServer} with simplest configuration
+     * Create {@link NatsServer} with simplest start able configuration
      *
      * @param port start port - common default port is 4222
      */
@@ -64,7 +67,7 @@ public class NatsServer implements DisposableBean {
     }
 
     /**
-     * Create custom {@link NatsServer} with simplest configuration
+     * Create custom {@link NatsServer} with simplest configuration {@link NatsServer#setNatsServerConfig(String...)}
      *
      * @param natsServerConfig passes the original parameters to the server. example: port:4222, user:admin, password:admin
      */
@@ -72,6 +75,11 @@ public class NatsServer implements DisposableBean {
         this.setNatsServerConfig(natsServerConfig);
     }
 
+    /**
+     * GetNatServerConfig
+     *
+     * @return the {@link NatsServer} configuration but not the config of the real PID
+     */
     public Map<String, String> getNatsServerConfig() {
         return natsServerConfig;
     }
@@ -113,9 +121,16 @@ public class NatsServer implements DisposableBean {
         this.natsServerConfig = natsServerConfig;
     }
 
+    /**
+     * Starts the server in {@link ProcessBuilder} with the given parameterConfig {@link NatsServer#setNatsServerConfig(String...)}
+     *
+     * @throws IOException      if {@link NatsServer} is not found or unsupported on the {@link OperatingSystem}
+     * @throws BindException    if port is already taken
+     * @throws RuntimeException with {@link ConnectException} if {@link NatsServer} is not starting
+     */
     public void start() throws IOException {
         if (process != null) {
-            LOG.error("[{}] is already running", name);
+            LOG.error("[{}] is already running", BEAN_NAME);
             return;
         }
         if (!waitForPort(true)) {
@@ -124,10 +139,10 @@ public class NatsServer implements DisposableBean {
         Path natsServerPath = getNatsServerPath();
         Path os = natsServerPath.getParent().getFileName();
         Path version = natsServerPath.getParent().getParent().getFileName();
-        LOG.info("Starting [{}] version [{}-{}]", name, version, os);
+        LOG.info("Starting [{}] version [{}-{}]", BEAN_NAME, version, os);
 
         try {
-            Files.setPosixFilePermissions(natsServerPath, EnumSet.of(OTHERS_EXECUTE, GROUP_EXECUTE, OWNER_EXECUTE));
+            Files.setPosixFilePermissions(natsServerPath, EnumSet.of(OTHERS_EXECUTE, GROUP_EXECUTE, OWNER_EXECUTE, OTHERS_READ, GROUP_READ, OWNER_READ));
         } catch (IOException e) {
             LOG.warn("Could not set permission to file [{}]", Arrays.asList(Files.getPosixFilePermissions(natsServerPath).toArray()), e.getMessage());
         }
@@ -154,15 +169,21 @@ public class NatsServer implements DisposableBean {
 
         process = builder.start();
         if (!waitForPort(false)) {
-            throw new RuntimeException(new ConnectException(name + "failed to start."));
+            throw new RuntimeException(new ConnectException(BEAN_NAME + "failed to start."));
         }
-        LOG.info("Started [{}] version [{}-{}]", name, version, os);
+        LOG.info("Started [{}] version [{}-{}]", BEAN_NAME, version, os);
     }
 
+    /**
+     * Stops the {@link ProcessBuilder} and kills the {@link NatsServer}
+     * Only a log error will occur if the {@link NatsServer} were never started
+     *
+     * @throws InterruptedException if shutdown is interrupted
+     */
     public void stop() {
-        LOG.info("Stopping [{}]", name);
+        LOG.info("Stopping [{}]", BEAN_NAME);
         if (process == null) {
-            LOG.warn("Could not stop [{}] cause cant find process", name);
+            LOG.warn("Could not stop [{}] cause cant find process", BEAN_NAME);
             return;
         }
         process.destroy();
@@ -172,10 +193,16 @@ public class NatsServer implements DisposableBean {
         } catch (InterruptedException e) {
             LOG.error("Shutdown was interrupted", e);
         } finally {
-            LOG.info("Stopped [{}]", name);
+            LOG.info("Stopped [{}]", BEAN_NAME);
         }
     }
 
+    /**
+     * Gets the port out of the configuration not from the real PID
+     *
+     * @return configured port of the server
+     * @throws RuntimeException with {@link ServiceConfigurationError} when there is no port configured
+     */
     public int getPort() {
         for (String key : new String[]{"port", "--port", "-p"}) {
             String port = natsServerConfig.get(key);
@@ -183,22 +210,17 @@ public class NatsServer implements DisposableBean {
                 return Integer.valueOf(port);
             }
         }
-        throw new RuntimeException(new ServiceConfigurationError("Could not initialise port" + name));
+        throw new RuntimeException(new ServiceConfigurationError("Could not initialise port" + BEAN_NAME));
     }
 
+    /**
+     * Simply stops the {@link NatsServer}
+     *
+     * @see NatsServer#stop()
+     */
     @Override
     public void destroy() {
         stop();
-    }
-
-    private static boolean isPortAvailable(int port) {
-        try {
-            new Socket("localhost", port).close();
-            return false;
-        } catch (IOException e) {
-            return true;
-        }
-
     }
 
     private boolean waitForPort(boolean isFree) {
@@ -214,12 +236,41 @@ public class NatsServer implements DisposableBean {
         return false;
     }
 
+    private static boolean isPortAvailable(int port) {
+        try {
+            new Socket("localhost", port).close();
+            return false;
+        } catch (IOException e) {
+            return true;
+        }
+    }
+
     /**
      * Gets Nats server path
      *
-     * @return Resource/{NATS_SERVER_VERSION}/{OPERATING_SYSTEM}/{SIMPLE_CLASS_NAME}
+     * @return Resource/{SIMPLE_CLASS_NAME}/{NATS_SERVER_VERSION}/{OPERATING_SYSTEM}/{SIMPLE_CLASS_NAME}
      */
     private Path getNatsServerPath() {
-        return Paths.get(requireNonNull(getClass().getClassLoader().getResource(NATS_SERVER_VERSION + File.separator + OPERATING_SYSTEM.toString().toLowerCase() + File.separator + name.toLowerCase() + (OPERATING_SYSTEM == WINDOWS ? ".exe" : ""))).getFile());
+        StringBuilder path = new StringBuilder();
+        path.append(BEAN_NAME.toLowerCase()).append(File.separator);
+        path.append(NATS_SERVER_VERSION).append(File.separator);
+        path.append(OPERATING_SYSTEM.toString().toLowerCase()).append(File.separator);
+        path.append(BEAN_NAME.toLowerCase()).append((OPERATING_SYSTEM == WINDOWS ? ".exe" : ""));
+        return Paths.get(requireNonNull(getClass().getClassLoader().getResource(path.toString())).getFile());
+    }
+
+    @Override
+    public String toString() {
+        int port;
+        try {
+            port = getPort();
+        } catch (Exception e) {
+            port = -1;
+        }
+        return "NatsServer{" +
+                "NATS_SERVER_VERSION=" + NATS_SERVER_VERSION +
+                ", OPERATING_SYSTEM=" + OPERATING_SYSTEM +
+                ", port=" + port +
+                '}';
     }
 }
