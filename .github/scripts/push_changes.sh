7069616e6b39cbb5460ca336482191f20ebf7e38
Yuna Morgenstern <yuna-@web.de>
2020-12-12 12:43:57 +0100

    updated tag script
    Former-commit-id: 679ca1970c8dea56423c6dbeccc7fee3c85c2465

--- .github/scripts/push_changes.sh
+++ .github/scripts/push_changes.sh
@@ -21,7 +21,7 @@ if [[ $(git status --porcelain) ]]; then
   echo "Pushing new git changes"
   git add .
   git commit -am "Updated dependencies"
-  git push origin  -f
+  git push origin -f
 else
   echo "No git changes to push"
 fi
@@ -30,6 +30,7 @@ TAG_OLD=$(git describe --tag --always --abbrev=0)
 VERSION=$(./mvnw -q -Dexec.executable=echo -Dexec.args="\${project.version}" --non-recursive exec:exec)
 git tag "${VERSION}" &>/dev/null || true
 TAG_NEW=$(git describe --tag --always --abbrev=0)
+echo "TAG_OLD [${TAG_OLD}] <> TAG_NEW [${TAG_NEW}] && TAG_NEW [${TAG_NEW}] <> VERSION [${VERSION}]"
 if [ "${TAG_OLD}" == "${TAG_NEW}" ] && [ "${TAG_NEW}" == "${VERSION}" ]; then
   echo "Tag already exists"
 else
@@ -37,5 +38,5 @@ else
   git branch "release" &>/dev/null || true
   # new branch as trigger for new release
   git push origin --all -u -f || true
-  git push origin --tags  -f || true
+  git push origin --tags -f || true
 fi
