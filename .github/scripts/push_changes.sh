54c1c69f4f1024cc71c305c6ac174a015cf07951
Yuna Morgenstern <yuna-@web.de>
2020-12-12 13:35:51 +0100

    updated tag script
    Former-commit-id: e0dd4187baae3517660c0e3f37ffa9bac3a1cf8a

--- .github/scripts/push_changes.sh
+++ .github/scripts/push_changes.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+git --version
 increment_version="$(dirname "$0")/increment_version.sh"
 cd "$(dirname "$0")/../../" || exit 1
 # REMOVE COVERAGE FILES
@@ -21,21 +22,24 @@ if [[ $(git status --porcelain) ]]; then
   echo "Pushing new git changes"
   git add .
   git commit -am "Updated dependencies"
-  git push origin  -f
+  git push origin -f
 else
   echo "No git changes to push"
 fi
 # CREATE TAG
+git fetch --all --force || true
+git fetch --tags --force || true
 TAG_OLD=$(git describe --tag --always --abbrev=0)
 VERSION=$(./mvnw -q -Dexec.executable=echo -Dexec.args="\${project.version}" --non-recursive exec:exec)
 git tag "${VERSION}" &>/dev/null || true
 TAG_NEW=$(git describe --tag --always --abbrev=0)
-if [ "${TAG_OLD}" == "${TAG_NEW}" ] && [ "${TAG_NEW}" == "${VERSION}" ]; then
+echo "TAG_OLD [${TAG_OLD}] <> TAG_NEW [${TAG_NEW}]"
+if [ "${TAG_OLD}" == "${TAG_NEW}" ]; then
   echo "Tag already exists"
 else
   echo "New tag created ${TAG_NEW}"
   git branch "release" &>/dev/null || true
   # new branch as trigger for new release
   git push origin --all -u -f || true
-  git push origin --tags  -f || true
+  git push origin --tags -f || true
 fi
