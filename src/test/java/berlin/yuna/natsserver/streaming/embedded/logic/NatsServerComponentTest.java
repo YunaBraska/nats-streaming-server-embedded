7dce917da4a81e9ed7d9047c114696a10f3a3a6b
Yuna <io@yuna.berlin>
2026-08-17 22:31:01 +0200

    test: isolate streaming server ports (#47)
    * test: isolate streaming server ports
    
    * ci: update shared workflows

--- src/test/java/berlin/yuna/natsserver/streaming/embedded/logic/NatsServerComponentTest.java
+++ src/test/java/berlin/yuna/natsserver/streaming/embedded/logic/NatsServerComponentTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.net.Socket;
 
 import static berlin.yuna.natsserver.config.NatsStreamingConfig.PORT;
@@ -29,22 +30,23 @@ class NatsServerComponentTest {
 
     @Test
     @DisplayName("Start server on a random port")
-    void natsServer_shouldStartOnARandomPort() {
+    void natsServer_shouldStartOnARandomPort() throws IOException {
         assertThat(natsServer, is(notNullValue()));
         assertThat(natsServer.port(), is(greaterThan((int) PORT.defaultValue())));
         assertThat(natsServer.pid(), is(greaterThan(-1)));
+        assertNatsServerReachable(natsServer);
     }
 
     @Test
     @DisplayName("Port config with double dash")
     void secondNatsServer_withDoubleDotSeparatedProperty_shouldStartSuccessful() {
-        assertNatsServerStart(4225, "--port", "4225");
+        assertNatsServerStart("--port", "-1");
     }
 
     @Test
     @DisplayName("Port config without dashes")
     void secondNatsServer_withOutMinusProperty_shouldStartSuccessful() {
-        assertNatsServerStart(4226, "port", "4226");
+        assertNatsServerStart("port", "-1");
     }
 
     @Test
@@ -52,7 +54,7 @@ class NatsServerComponentTest {
     void secondNatsServer_withInvalidProperty_shouldFailToStart() {
         assertThrows(
                 IllegalArgumentException.class,
-                () -> assertNatsServerStart(4228, "p", "4228"),
+                () -> assertNatsServerStart("p", "-1"),
                 "No enum constant"
         );
     }
@@ -64,11 +66,15 @@ class NatsServerComponentTest {
         assertThat(serverString, containsString(String.valueOf(natsServer.port())));
     }
 
-    private void assertNatsServerStart(final int port, final String... config) {
+    private void assertNatsServerStart(final String... config) {
         try (final NatsStreamingServer natsServer = new NatsStreamingServer(natsStreamingBuilder().timeoutMs(10000).config(config).build())) {
-            new Socket("localhost", port).close();
+            assertNatsServerReachable(natsServer);
         } catch (Exception e) {
             throw new IllegalArgumentException(e);
         }
+    }
+
+    private void assertNatsServerReachable(final NatsStreamingServer server) throws IOException {
+        new Socket("localhost", server.port()).close();
     }
 }
